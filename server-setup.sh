229f150d03c85aae0616969a56d303b4e04ff324
Tim Abell <tim@timwise.co.uk>
2017-08-17 00:13:32 +0100

    Volume config for proxy + compose upgrade
    Use latest compose to support 2.1 yml format.
    
    $ docker-compose --version && docker --version
    docker-compose version 1.15.0, build e12f3b9
    Docker version 1.12.6, build 78d1802

--- server-setup.sh
+++ server-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# For setting up docker on bytemark's symbiosis OS on shared hosting.
-# https://symbiosis.bytemark.co.uk/
+# setup on Ubuntu 16.04 LTS, hosted on bytemark vps
 # https://docs.docker.com/compose/install/#install-compose-on-linux-systems
 # needs root
 apt install docker curl
-dockerComposeVersion=1.5.2
+dockerComposeVersion=1.15.0
+# https://stackoverflow.com/a/42273713/10245
+apt-get purge docker-compose
 curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 udo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
