7a5d7955c229eefea3ffd464760f02b5c4ce2f44
Tim Abell <tim@timwise.co.uk>
2017-08-16 22:47:32 +0100

    Cleanup - remove docker volumes
    Prep for nginx config volume

--- docker-rm-all.sh
+++ docker-rm-all.sh
@@ -2,3 +2,5 @@
 # https://stackoverflow.com/a/30311197/10245
 docker rm -f $(docker ps -a -q)
 docker rmi -f  $(docker images -q)
+# https://stackoverflow.com/a/40654726/10245
+docker volume rm $(docker volume ls -q --filter dangling=true)
