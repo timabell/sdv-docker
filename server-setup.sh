63e1dc963b4cf22e0c491ceb82ce1545284984b6
Tim Abell <tim@timwise.co.uk>
2017-08-17 00:16:47 +0100

    Typo - remove sudo prefix in root script

--- server-setup.sh
+++ server-setup.sh
@@ -7,5 +7,5 @@ dockerComposeVersion=1.15.0
 # https://stackoverflow.com/a/42273713/10245
 apt-get purge docker-compose
 curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-udo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose --version
