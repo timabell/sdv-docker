68d00105cab848669119f527406b86ba797197f6
Tim Abell <tim@timwise.co.uk>
2017-12-07 17:57:58 +0000

    Local build direct from source tree build
    This is for a private build of sdv for testing before publishing
    so this isn't useful for anyone other than me.

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 mkdir tmp
-curl -L -o tmp/sdv.zip https://www.dropbox.com/s/0q3sznge2bfos3x/sdv.zip?dl=0
 cd tmp
-unzip sdv.zip
+cp -r /home/tim/repo/go/src/bitbucket.org/timabell/sql-data-viewer/package/sdv/ sdv
 cd ..
 docker build . -t timabell/sdv
 rm -rf tmp
