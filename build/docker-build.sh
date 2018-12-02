c46cb45bf5991c670f97f7a7dfd3e22a9ee90674
Tim Abell <tim@timwise.co.uk>
2018-12-02 22:11:39 +0000

    Name change to sse
    Use local copy of chinook to save messing around with dropbox

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -2,6 +2,7 @@
 mkdir tmp
 cd tmp
 cp -r /home/tim/repo/go/src/bitbucket.org/timabell/sql-data-viewer/package/sse/ sse
+cp "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" .
 cd ..
 docker build . -t timabell/sdv
 rm -rf tmp
