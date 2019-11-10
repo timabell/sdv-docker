3d7789c5d7faa193f993a1a7b337702d3d393781
Tim Abell <tim@timwise.co.uk>
2019-11-10 16:21:41 +0000

    update paths

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir tmp
 cd tmp
-cp -r /home/tim/repo/go/src/bitbucket.org/timabell/sql-data-viewer/package/sse/ sse
+cp -r /home/tim/repo/schema-explorer/package/sse/ sse
 cp "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" .
 cd ..
 docker build . -t timabell/sdv
